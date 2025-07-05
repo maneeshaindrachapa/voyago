f76bd62fdf06bb3f9057e9b8facf2f4c716d8b4c
maneeshaindrachapa <maneeshaindrachapa@gmail.com>
2025-07-05 18:02:06 +0200

    fixed ui on mobile devices

--- frontend/src/pages/AuthenticationPage.tsx
+++ frontend/src/pages/AuthenticationPage.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { UserAuthForm } from '../components/AuthForm';
 
 export default function AuthenticationPage() {
-  const { isSignedIn } = useUser(); // Get authentication state
+  const { isSignedIn } = useUser();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,34 +15,35 @@ export default function AuthenticationPage() {
   }, [isSignedIn, navigate]);
 
   return (
-    <>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <img
-              src="./icons/logo-dark.png"
-              width={25}
-              height={25}
-              alt="Authentication"
-              className="block dark:hidden"
-            />
-            <img
-              src="./icons/logo-light.png"
-              width={25}
-              height={25}
-              alt="Authentication"
-              className="hidden dark:block"
-            />
-            &nbsp;Voyago
-          </div>
-        </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <UserAuthForm />
-          </div>
+    <div className="flex min-h-screen flex-col md:flex-row">
+      {/* Sidebar for large screens */}
+      <div className="hidden md:flex w-full md:w-1/2 flex-col bg-muted p-10 text-white dark:border-r relative">
+        <div className="absolute inset-0 bg-zinc-900" />
+        <div className="relative z-20 flex items-center text-lg font-medium">
+          <img
+            src="./icons/logo-dark.png"
+            width={25}
+            height={25}
+            alt="Authentication"
+            className="block dark:hidden"
+          />
+          <img
+            src="./icons/logo-light.png"
+            width={25}
+            height={25}
+            alt="Authentication"
+            className="hidden dark:block"
+          />
+          &nbsp;Voyago
         </div>
       </div>
-    </>
+
+      {/* Main content for all screens */}
+      <div className="flex flex-1 items-center justify-center p-4 md:p-8">
+        <div className="w-full max-w-md space-y-6">
+          <UserAuthForm />
+        </div>
+      </div>
+    </div>
   );
 }
